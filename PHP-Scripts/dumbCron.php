9957550cbb010f423b6e0c7048ecf4ff47b9ea41
unknown <solinski.mfs@gmail.com>
2018-10-20 18:43:17 +0200

    Add email sending to CRON

--- PHP-Scripts/dumbCron.php
+++ PHP-Scripts/dumbCron.php
@@ -4,14 +4,13 @@ require 'database.php';
 require 'getPage.php';
 
 $hour = date('H:i:s');
+$content = getPage('https://www.x-kom.pl');
+require 'patern.php';
+
+$mOldPrice = str_replace(',', '.', $mOldPrice['old_price'][0]);
+$mNewPrice = str_replace(',', '.', $mNewPrice['new_price'][0]);
 
 if (($hour >= '09:40:00' && $hour <= '09:50:00') || ($hour >= '21:40:00' && $hour <= '21:50:00')) {
-    $content = getPage('https://www.x-kom.pl');
-    require 'patern.php';
-
-    $mOldPrice = str_replace(',', '.', $mOldPrice['old_price'][0]);
-    $mNewPrice = str_replace(',', '.', $mNewPrice['new_price'][0]);
-
     $query = $db->prepare('INSERT INTO sold_product VALUES (NULL, :name, :photo, :old_price, :new_price, :date)');
     $query->bindValue(':name', $mNname['name'][0], PDO::PARAM_STR);
     $query->bindValue(':photo', $mPhoto['photo'][0], PDO::PARAM_STR);
@@ -20,4 +19,16 @@ if (($hour >= '09:40:00' && $hour <= '09:50:00') || ($hour >= '21:40:00' && $hou
     $query->bindValue(':date', date('Y-m-d'), PDO::PARAM_STR);
 
     $query->execute();
+}
+
+if (($hour >= '09:55:00' && $hour <= '10:05:00') || ($hour >= '21:55:00' && $hour <= '22:05:00')) {
+    $to = 'solinski.mfs@gmail.com';
+    $subject = 'X-kom: ' . $mNname['name'][0];
+    $message = 'Nowa cena: ' . $mNewPrice . 'zl Stara cena: ' . $mOldPrice . 'zl
+Rabat: ' . ($mOldPrice - $mNewPrice) . 'zl';
+    $headers = 'From: bot@psolinski.cba.pl' . "\r\n" .
+        'Reply-To: bot@psolinski.cba.pl' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
 }
